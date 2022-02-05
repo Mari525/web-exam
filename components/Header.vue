718def273b688819fa2816876be67aa695f0aae6
Mari <sherst.neva.maria@ya.ru>
2022-02-05 13:18:13 +0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -5,8 +5,8 @@
       class="d-flex"
       >	
         <v-img :src="require('~/static/earthquake_img.jpg')" class="mr-3 ml-5" height="50" width="50" />
-        <p class="mr-10 text-uppercase">Илонус</p>
-        <p>Регистрация погодных событий на Марсе "Илонус"</p>
+        <p class="mr-16 mt-4 text-uppercase font-weight-medium">Илонус</p>
+        <p class="mt-4">Регистрация погодных событий на Марсе "Илонус"</p>
       </v-app-bar>
     </v-container>
 </template>
